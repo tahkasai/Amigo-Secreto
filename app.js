b59f2c78754f1d1aa2a86f851d88e9bd752970bd
Tainá Kasai <tainakasai01@gmail.com>
2025-09-22 18:41:45 -0300

    feat: armazenando dados array

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// variáveis
+let nomes = [];
+
+// Adicionando amigo
+function adicionarAmigo(){
+    let nome = document.querySelector('#amigo').value;
+    nomes.push(nome);
+
+    console.log(nomes);
+    limparCampo();
+}
+
+// Limpar campo
+function limparCampo(){
+    document.querySelector('#amigo').value = '';
+}
